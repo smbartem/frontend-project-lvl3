af0e39b465d26ced08ba1de299bec86e2622fdc0
smbartem <artembass88@yandex.ru>
2021-02-12 17:48:43 +0300

    refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: process.env.NODE_ENV || 'development',
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'code'),
     filename: 'bundle.js',
   },
   module: {
